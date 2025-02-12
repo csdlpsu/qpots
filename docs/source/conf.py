758eafa51c53d4b98cc2decad7ce635da4ed8e0d
Kade Carlson <carlsonkade3232@gmail.com>
2025-02-11 21:46:38 -0500

    mocking matlab engine for rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,6 +5,10 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
+from unittest.mock import MagicMock
+
+MOCK_MODULES = ["matlab.engine"]
+sys.modules.update((mod_name, MagicMock()) for mod_name in MOCK_MODULES)
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
