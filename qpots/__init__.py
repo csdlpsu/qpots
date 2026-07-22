54fa2d663da2f554d0e38ae160e69b3831272682
akr6198 <akr6198@psu.edu>
2026-07-22 08:27:57 -0400

    Define the supported public package interface

--- qpots/__init__.py
+++ qpots/__init__.py
@@ -1,7 +1,8 @@
-"""
-qPOTS package
-"""
+"""Public package interface for qPOTS."""
 
+from importlib.metadata import PackageNotFoundError, version
+
+from qpots.acquisition import Acquisition
 from qpots.benchmark_registry import available_benchmarks, create_benchmark
 from qpots.config import (
     DEFAULT_DEVICE,
@@ -11,25 +12,36 @@ from qpots.config import (
     set_default_runtime,
 )
 from qpots.function import EvaluationResult, Function
+from qpots.model_object import ModelObject
 from qpots.runner import (
     IterationResult,
     OptimizationResult,
     QPOTSConfig,
     QPOTSRunner,
 )
+from qpots.tsemo_runner import TSEMORunner
+
+try:
+    __version__ = version("qpots")
+except PackageNotFoundError:  # pragma: no cover - source tree without installed metadata
+    __version__ = "0+unknown"
 
 __all__ = [
+    "__version__",
+    "Acquisition",
     "DEFAULT_DEVICE",
     "DEFAULT_DTYPE",
-    "RuntimeConfig",
-    "get_default_runtime",
-    "set_default_runtime",
     "EvaluationResult",
     "Function",
-    "available_benchmarks",
-    "create_benchmark",
     "IterationResult",
+    "ModelObject",
     "OptimizationResult",
     "QPOTSConfig",
     "QPOTSRunner",
+    "RuntimeConfig",
+    "TSEMORunner",
+    "available_benchmarks",
+    "create_benchmark",
+    "get_default_runtime",
+    "set_default_runtime",
 ]
